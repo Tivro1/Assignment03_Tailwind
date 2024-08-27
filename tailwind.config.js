99dcc174104b34272187233f4e95eb6b1ab79da9
unknown <tivro1040@gmail.com>
2024-08-27 22:59:24 +0600

    add all

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      screens: {
+        'xs': '480px',   // Extra small devices
+        'sm': '375px',   // Small devices (override default)
+        'md': '800px',   // Medium devices (custom size)
+        'lg': '1024px',  // Large devices (default)
+        'xl': '1280px',  // Extra large devices (default)
+        '2xl': '1440px', // 2x Extra large devices (custom size)
+      },
+    },
+    plugins: [],
+  }
+  
